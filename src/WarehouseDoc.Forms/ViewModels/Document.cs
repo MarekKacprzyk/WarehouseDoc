87ee6c8c754cc0938976b08ebe74e534cb247832
Skynet <Marek>
2022-01-09 15:35:40 +0100

    init gui project

---
+++ src/WarehouseDoc.Forms/ViewModels/Document.cs
@@ -0,0 +1,25 @@
+﻿namespace WarehouseDoc.Forms.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Document
+    {
+        public string Header { get; set; }
+        public Guid ClientId { get; set; }
+        public string Name { get; set; }
+        public decimal GrossPrice { get; set; }
+        public decimal NetPrice { get; set; }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public decimal GrossPrice { get; set; }
+        public decimal NetPrice { get; set; }
+    }
+}
